e29f0c309345f36b4956f14fa46124ee19000fe1
Mithun Mandal <mithun12000@gmail.com>
2016-02-22 14:20:19 +0530

    update namespace

--- src/models/SysparamsTrashSearch.php
+++ src/models/SysparamsTrashSearch.php
@@ -5,7 +5,7 @@ namespace mithun\usermanage\models;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use mithun\parametricfilter\models\SystemParams;
+use mithun\usermanage\models\SystemParams;
 
 /**
  * SysparamsTrashSearch represents the model behind the search form about `mithun\parametricfilter\models\SystemParams`.
